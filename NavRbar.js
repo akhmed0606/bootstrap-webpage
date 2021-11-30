d25e448dcc4b32f8baeca92dd6fc15c5f07c8c5b
Akhmed Nalgiev <77641004+akhmed0606@users.noreply.github.com>
2021-11-29 21:32:00 -0500

    Update NavRbar.js

--- NavRbar.js
+++ NavRbar.js
@@ -1,7 +1,16 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+const ancor = document.querySelector('.ancor')
+const ancor2 = document.querySelector('.ancor2')
+const ancor3 = document.querySelector('.ancor3')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+ancor.addEventListener('click', () => container.classList.remove('show-nav'))
+
+ancor2.addEventListener('click', () => container.classList.remove('show-nav'))
+
+ancor3.addEventListener('click', () => container.classList.remove('show-nav'))
